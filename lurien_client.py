e83826b433d32f01514cbc5c99a2e22867f2251c
Lucas Towers <luctowers@gmail.com>
2023-02-24 01:22:54 -0800

    Add basic save watching mechanics

--- lurien_client.py
+++ lurien_client.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import json
+import uuid
 import tkinter as tk
 from tkinter import scrolledtext
 
@@ -27,23 +28,24 @@ def locate_saves(save_dir):
             if name.endswith(".dat"):
                 yield os.path.join(root, name)
 
-def ensure_lurien_save_dir():
+def ensure_lurien_persist_dir():
     # TODO: MAKE THIS MULTIPLATFORM
     save_dir = os.path.join(os.environ["USERPROFILE"], "AppData\\LocalLow\\Lurien")
     os.makedirs(save_dir, exist_ok=True)
     return save_dir
 
-def get_lurien_save_file():
-    save_dir = ensure_lurien_save_dir()
+def get_lurien_persist_file():
+    save_dir = ensure_lurien_persist_dir()
     return os.path.join(save_dir, "lurien.json")
 
-def save_lurien_data(data):
-    with open(get_lurien_save_file(), "w") as file:
-        json.dump(data, file)
+def save_lurien_persist(data):
+    with open(get_lurien_persist_file(), "w") as file:
+        json.dump(data, file, indent="\t")
 
-def get_lurien_data():
+def get_lurien_persist():
+    # TODO: HANDLE INVALID PERSIST FILE
     try:
-        with open(get_lurien_save_file(), "r") as file:
+        with open(get_lurien_persist_file(), "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return None
@@ -65,10 +67,40 @@ class LurienApp(tk.Frame):
         self.log_text.configure(state="disabled")
         self.log_text.pack(anchor="w")
 
-        self.watch()
+        self.init_persist()
+        self.log("starting lurien client id = %s" % self.persist["clientId"])
 
-    def watch(self):
-        self.log("Lurien is watching...")
+        self.watch(first_watch=True)
+
+    def init_persist(self):
+        self.persist = get_lurien_persist()
+        if self.persist is None:
+            self.persist = {}
+            self.persist["saves"] = {}
+            self.persist["clientId"] = str(uuid.uuid4())
+            self.save_persist()
+    
+    def save_persist(self):
+        save_lurien_persist(self.persist)
+
+    def watch(self, first_watch=False):
+        persist_updated = False
+        for save in locate_saves(locate_hollow_knight_save_dir()):
+            mod_time = os.path.getmtime(save)
+            if save in self.persist["saves"]:
+                persist_mod_time = self.persist["saves"][save]["mtime"]
+                if persist_mod_time < mod_time:
+                    print("CHANGED " + save)
+                if persist_mod_time != mod_time:
+                    persist_updated = True
+            else:
+                persist_updated = True    
+                self.persist["saves"][save] = {}
+                if not first_watch:
+                    print("NEW " + save)
+            self.persist["saves"][save]["mtime"] = mod_time
+        if persist_updated:
+            self.save_persist()
         self.after(1000, self.watch)
 
     def log(self, text):
@@ -79,7 +111,7 @@ class LurienApp(tk.Frame):
         
 def main():
     root = tk.Tk()
-    root.title("lurien")
+    root.title("Project Lurien")
     app = LurienApp(root)
     app.pack()
     root.mainloop()
