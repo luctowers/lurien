20a68fbfb483758e5eb40f56d8d732cbe2fb2a73
Lucas Towers <luctowers@gmail.com>
2023-02-24 02:17:23 -0800

    Support MacOS

--- lurien_client.py
+++ lurien_client.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 import json
 import uuid
@@ -7,9 +8,18 @@ from tkinter import scrolledtext
 
 # TODO: make sure there is a way to disable the client from the server
 
+class UnsupportedPlatformException(Exception):
+    def __init__(self):            
+        super().__init__("unsupported platform '%s'" % platform.system())
+
 def launch_steam_game(id):
     # TODO: MAKE THIS MULTIPLATFORM
-    subprocess.call("C:\\Program Files (x86)\\Steam\\\Steam.exe -applaunch %s" % id)
+    if platform.system() == "Windows":
+        subprocess.call(["start", "steam://run/%s" % id], shell=True)
+    elif platform.system() == "Darwin":
+        subprocess.call(["open", "steam://run/%s" % id])
+    else:
+        raise UnsupportedPlatformException()
 
 def launch_hollow_knight():
     launch_steam_game("367520")
@@ -19,7 +29,14 @@ def launch_silksong():
 
 def locate_hollow_knight_save_dir():
     # TODO: MAKE THIS MULTIPLATFORM https://store.steampowered.com/news/app/367520/view/3406429723545099871
-    return os.path.join(os.environ["USERPROFILE"], "AppData\\LocalLow\\Team Cherry\\Hollow Knight")
+    if platform.system() == "Windows":
+        save_dir = "~\\AppData\\LocalLow\\Team Cherry\\Hollow Knight"
+    if platform.system() == "Darwin":
+        save_dir = "~/Library/Application Support/unity.Team Cherry.Hollow Knight"
+    else:
+        raise UnsupportedPlatformException()
+    save_dir = os.path.expanduser(save_dir)
+    return save_dir
 
 def locate_saves(save_dir):
     # TODO: MAKE SURE THIS DOESN'T FAIL WHEN SAVE DIR DOESN'T EXIST
@@ -30,7 +47,13 @@ def locate_saves(save_dir):
 
 def ensure_lurien_persist_dir():
     # TODO: MAKE THIS MULTIPLATFORM
-    save_dir = os.path.join(os.environ["USERPROFILE"], "AppData\\LocalLow\\Lurien")
+    if platform.system() == "Windows":
+        save_dir = "~\\AppData\\LocalLow\\Lurien"
+    if platform.system() == "Darwin":
+        save_dir = "~/Library/Application Support/net.lurien"
+    else:
+        raise UnsupportedPlatformException()
+    save_dir = os.path.expanduser(save_dir)
     os.makedirs(save_dir, exist_ok=True)
     return save_dir
 
