ffe27e60c9db96f8a304ffff86fee1982b284340
Lucas Towers <luctowers@gmail.com>
2023-02-24 18:27:21 -0800

    Little bit of cleanup

--- lurien_client.py
+++ lurien_client.py
@@ -21,10 +21,13 @@ VERSIONED_SAVE_PATTERN = re.compile(r".*_[0-9]*\.[0-9]*(\.[0-9]*)*\.dat")
 def is_regular_save_file(filename):
     return ALL_SAVE_PATTERN.fullmatch(filename) and not VERSIONED_SAVE_PATTERN.fullmatch(filename)
 
-def staged_save_file_name(save, mod_time):
-    savename = os.path.basename(save)[:-4]
+def shorten_save_file(save):
+    return os.path.join("~hk", os.path.relpath(save, locate_hollow_knight_save_dir()))
+
+def staged_save_file_name(save, mod_time, game_dir):
+    savename = os.path.relpath(save, game_dir)[:-4].replace(os.sep, "--")
     timestamp = datetime.utcfromtimestamp(mod_time).strftime("%Y-%m-%d--%H-%M-%S")
-    return "%s--%s.dat" % (savename, timestamp)
+    return "%s---%s.dat" % (savename, timestamp)
 
 def launch_steam_game(id):
     # TODO: MAKE THIS MULTIPLATFORM
@@ -98,7 +101,7 @@ class LurienApp(tk.Frame):
         super().__init__(parent)
 
         launch_frame = tk.LabelFrame(self, text="steam launch")
-        launch_frame.pack(anchor="w")
+        launch_frame.grid(row=0, column=0, sticky="w")
 
         hk_button = tk.Button(launch_frame, text="Hollow Knight", command=launch_hollow_knight)
         ss_button = tk.Button(launch_frame, text="Silksong", command=launch_silksong)
@@ -107,14 +110,19 @@ class LurienApp(tk.Frame):
 
         self.log_text = scrolledtext.ScrolledText(self)
         self.log_text.configure(state="disabled")
-        self.log_text.pack(anchor="w")
+        self.log_text.grid(row=1, column=0, sticky="nswe")
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
         self.init_persist()
-        self.log("starting lurien client id = %s" % self.persist["clientId"])
+        self.log("starting lurien client id: %s" % self.persist["clientId"])
+        self.log("hk save directory: %s" % locate_hollow_knight_save_dir())
+        self.log("lurien is watching...")
 
         self.watch(first_watch=True)
 
     def init_persist(self):
+        self.log("performing first time setup")
         self.persist = get_lurien_persist()
         if self.persist is None:
             self.persist = {}
@@ -132,6 +140,7 @@ class LurienApp(tk.Frame):
             if save in self.persist["saves"]:
                 persist_mod_time = self.persist["saves"][save]["mtime"]
                 if persist_mod_time < mod_time:
+                    self.log("save change detected: %s" % shorten_save_file(save))
                     self.stage(save, mod_time)
                 if persist_mod_time != mod_time:
                     persist_updated = True
@@ -139,6 +148,7 @@ class LurienApp(tk.Frame):
                 persist_updated = True    
                 self.persist["saves"][save] = {}
                 if not first_watch:
+                    self.log("new save detected: %s" % shorten_save_file(save))
                     self.stage(save, mod_time)
             self.persist["saves"][save]["mtime"] = mod_time
         if persist_updated:
@@ -146,25 +156,28 @@ class LurienApp(tk.Frame):
         self.after(1000, self.watch)
     
     def stage(self, save, mod_time):
-        stagename = staged_save_file_name(save, mod_time)
+        stagename = staged_save_file_name(save, mod_time, locate_hollow_knight_save_dir())
+        self.log("staging file: %s" % stagename)
         stagedir = ensure_lurien_staging_dir()
         stagesave = os.path.join(stagedir, stagename)
         shutil.copyfile(save, stagesave)
 
     def log(self, text):
+        timestamp = datetime.now().replace(microsecond=0).isoformat()
         self.log_text.configure(state="normal")
-        self.log_text.insert("end", text + "\n")
+        self.log_text.insert("end", "[%s] %s\n" % (timestamp, text))
         self.log_text.configure(state="disabled")
         self.log_text.see("end")
         
 def main():
     root = tk.Tk()
     root.title("Project Lurien")
+    root.geometry("900x600")
     app = LurienApp(root)
-    app.pack()
+    app.grid(row=0, column=0, sticky="nswe")
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
     root.mainloop()
-    # print(get_lurien_data())
-    # save_lurien_data({"id":"123"})
 
 if __name__ == "__main__":
     main()
