361066300386ddcf36319852ad86b4929cf10876
Lucas Towers <luctowers@gmail.com>
2023-02-26 01:12:41 -0800

    Finalize initial intake prototype

--- intake/api.go
+++ intake/api.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"regexp"
 	"time"
 
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/google/uuid"
 	"github.com/luctowers/lurien/common"
@@ -32,13 +33,26 @@ type intakeHandler struct {
 }
 
 func (h *intakeHandler) Handle(i common.Input) (int, error) {
-	client := i.Params.ByName("client")
 	save := i.Params.ByName("save")
-	agent := i.Request.Header.Get("User-Agent")
+	client := i.Params.ByName("client")
+	saveSize := i.Request.ContentLength
+	agent := i.Request.UserAgent()
 
 	if agent != "lurien_client/1.0" {
 		i.Logger.Warn("unrecognized user-agent", zap.String("userAgent", agent))
 		return http.StatusBadRequest, errors.New("unrecognized user-agent")
+	}
+
+	if saveSize == 0 {
+		return http.StatusBadRequest, errors.New("content length zero is not allowed")
+	}
+	if saveSize < 0 { // -1 means unknown
+		return http.StatusBadRequest, errors.New("unkown content length is not allowed")
+	}
+	// TODO: make this configurable
+	if saveSize > 10_000_000 {
+		i.Logger.Warn("save size exceeds maximum", zap.Int64("saveSize", saveSize))
+		return http.StatusRequestEntityTooLarge, errors.New("save size exceeds maximum")
 	}
 
 	if !isValidUUID(client) {
@@ -55,7 +69,7 @@ func (h *intakeHandler) Handle(i common.Input) (int, error) {
 	saveTs := saveMatch[2]
 	saveExt := saveMatch[3]
 
-	_, err := time.Parse(h.saveTsLayout, saveTs)
+	saveTime, err := time.Parse(h.saveTsLayout, saveTs)
 	if err != nil {
 		i.Logger.Warn("failed to parse save time", zap.String("save", save), zap.String("saveTs", saveTs), zap.Error(err))
 		return http.StatusBadRequest, errors.New("failed to parse save time")
@@ -66,14 +80,29 @@ func (h *intakeHandler) Handle(i common.Input) (int, error) {
 		return http.StatusBadRequest, errors.New("unrecognized save extension")
 	}
 
-	result, err := h.s3c.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	// TODO: is this even needed since we checked content length???
+	saveBody := http.MaxBytesReader(i.Response, i.Request.Body, i.Request.ContentLength)
+
+	s3key := fmt.Sprintf("client=%s/%s", client, save)
+	_, err = h.s3c.PutObject(i.Request.Context(), &s3.PutObjectInput{
+		Bucket:        &h.s3bucket,
+		Key:           &s3key,
+		Body:          saveBody,
+		ContentLength: saveSize,
+	}, s3.WithAPIOptions(v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware))
 	if err != nil {
+		i.Logger.Error("failed to upload to s3", zap.Error(err))
 		return http.StatusInternalServerError, err
 	}
-	i.Logger.Info("listing buckets")
-	for _, bucket := range result.Buckets {
-		i.Logger.Info(*bucket.Name)
-	}
+
+	i.Logger.Debug(
+		"save upload completed",
+		zap.Time("saveTime", saveTime),
+		zap.String("save", save),
+		zap.String("client", client),
+		zap.String("s3bucket", h.s3bucket),
+		zap.String("s3key", s3key),
+	)
 
 	return http.StatusOK, nil
 }
