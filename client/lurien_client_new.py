38ba933dcf61a48eb05042ea600421950e3a8b75
Lucas Towers <luctowers@gmail.com>
2023-03-13 01:29:28 -0700

    Add a multithreaded work to the new client

--- client/lurien_client_new.py
+++ client/lurien_client_new.py
@@ -1,4 +1,5 @@
-import os, platform, subprocess, shutil, json, uuid, re, secrets
+import os, platform, subprocess, shutil, json, uuid, re, secrets, threading, queue
+import queue, time
 from datetime import datetime
 import tkinter as tk
 from tkinter import ttk
@@ -44,16 +45,47 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Project Lurien")
+        self.layout()
         self.ensure_user_consent()
         self.state = PeristentState()
+
+    def layout(self):
+        self.log_text = ScrolledText(self, padx=4, pady=4, wrap="word")
+        self.log_text.config(state="disabled")
+        self.log_text.pack()
     
     def ensure_user_consent(self):
         consent_popup = AgreementPopup(
             self,
-            agree_callback=lambda: self.deiconify(),
+            agree_callback=lambda: self.receive_user_consent(),
             decline_callback=lambda: self.destroy()
         )
         self.withdraw()
+    
+    def receive_user_consent(self):
+        self.deiconify()
+        self.queue = queue.Queue()
+        worker_thread = threading.Thread(target=Worker(self.state, self.queue))
+        worker_thread.start()
+        self.poll()
+    
+    def poll(self):
+        try:
+            while True:
+                message = self.queue.get_nowait()
+                self.log(message)
+                self.queue.task_done()
+        except queue.Empty:
+            pass
+        self.after(1000, self.poll)
+    
+    def log(self, text):
+        timestamp = datetime.now().replace(microsecond=0).isoformat()
+        self.log_text.configure(state="normal")
+        self.log_text.insert("end", "[%s] %s\n" % (timestamp, text))
+        self.log_text.configure(state="disabled")
+        self.log_text.see("end")
+        
 
 
 
@@ -98,7 +130,6 @@ class PeristentState:
     
     def boostrap(self):
         self.state = self.read()
-        print(self.state, self.path)
         if self.state is None:
             self.state = {
                 "clientId": str(uuid.uuid4()),
@@ -116,6 +147,28 @@ class PeristentState:
     def save(self):
         with open(self.path, "w") as file:
             json.dump(self.state, file, indent="\t")
+
+
+
+# WORKER
+
+
+
+class Worker:
+
+    def __init__(self, state, queue):
+        self.state = state
+        self.queue = queue
+        self.n = 0
+
+    def __call__(self):
+        self.run()
+
+    def run(self):
+        while True:
+            self.n += 1
+            time.sleep(0.5)
+            self.queue.put("yo%d" % self.n)
 
 
 
